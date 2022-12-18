17fed5b4a31596bf67314eb83e4443d9da6c9448
chandrashekar <cnchandarshekar3@gmail.com>
2022-12-18 12:53:05 +0530

    welcome to git

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>WELCOME TO GPT CPT</h1>
+        <div>
+          <form>
+            Name: <input type="text"></input> <br></br>
+            Address: <input type="text"></input><br></br>
+            Email : <input type="text"></input><br></br>
+            Phone-No : <input type="text"></input><br></br>
+            Register-Number : <input type="text"></input><br></br>
+            <button>Submit</button>
+          </form>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
